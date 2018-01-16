8cfa47e4b42aa4501c4587b78e2f2a6eef551f4d
Ashton Hellwig <ahellwig.dev@gmail.com>
2018-01-16 02:13:59 -0700

    updated test

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,13 +5,16 @@ const helpers = require('yeoman-test');
 
 describe('generator-elm-mdl:app', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers.run(path.join(__dirname, '../generators/app'));
   });
 
   it('creates files', () => {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+    assert.file(['dummyfile.txt']);
+    assert.file(['elm-package.json']);
+    assert.file(['src/Elm/Main.elm']);
+    assert.file(['src/Elm/Components/Models.elm']);
+    assert.file(['src/Elm/Components/Types.elm']);
+    assert.file(['src/Elm/Components/Update.elm']);
+    assert.file(['src/Elm/Components/Views.elm']);
   });
 });
