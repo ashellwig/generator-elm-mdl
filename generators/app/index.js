f5d1af571d2d5b1383a0a3327c17ce5b4a830eca
Ashton Hellwig <ahellwig.dev@gmail.com>
2018-01-16 02:06:54 -0700

    moved files

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,33 +4,41 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
+  // Yeoman Greeting
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the wicked ' + chalk.red('elm-mdl') + ' generator!'
-    ));
-/* Untoggle Line Comment Below for Prompts */ 
-    // const prompts = [{
-      // type: 'confirm',
-      // name: 'someAnswer',
-      // message: 'Would you like to enable this option?',
-      // default: true
-    // }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.log(yosay('Welcome to the ' + chalk.red('elm-mdl') + ' generator!'));
   }
 
+  // Copy Scaffold
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('elm-package.json'),
+      this.destinationPath('elm-package.json')
+    );
+    this.fs.copy(
+      this.templatePath('src/Elm/Main.elm'),
+      this.destinationPath('src/Elm/Main.elm')
+    );
+    this.fs.copy(
+      this.templatePath('src/Elm/Components/Models.elm'),
+      this.destinationPath('src/Elm/Components/Models.elm')
+    );
+    this.fs.copy(
+      this.templatePath('src/Elm/Components/Types.elm'),
+      this.destinationPath('src/Elm/Components/Types.elm')
+    );
+    this.fs.copy(
+      this.templatePath('src/Elm/Components/Update.elm'),
+      this.destinationPath('src/Elm/Components/Update.elm')
+    );
+    this.fs.copy(
+      this.templatePath('src/Elm/Components/Views.elm'),
+      this.destinationPath('src/Elm/Components/Views.elm')
     );
   }
 
+  // Install Dependencies
   install() {
-    this.installDependencies();
+    this.spawnCommand('elm-package', ['install']);
   }
 };
